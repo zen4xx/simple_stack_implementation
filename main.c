2fdf776b47fc0db29352a3500435c7caef6e1849
zntwpp <yarikshcher@gmail.com>
2025-03-01 13:49:06 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <stdbool.h>
-
-#define MAX_SIZE 16
+#include <stdlib.h>
 
 typedef struct{   
-    int arr[MAX_SIZE-1];
+    int* arr;
     int top;
+    int max_size;
+    bool is_dynamic;
 } Stack;
 
-void init(Stack* stack){
+void init(Stack* stack, int size, bool is_dynamic){
     stack->top = -1;
+    stack->max_size = size;
+    stack->arr = malloc(stack->max_size*sizeof(int));
+    stack->is_dynamic = is_dynamic;
+}
+
+void destroy(Stack* stack){
+    stack->top = -1;
+    free(stack->arr);
 }
 
 bool is_empty(Stack* stack){
@@ -17,7 +26,7 @@ bool is_empty(Stack* stack){
 }
 
 bool is_full(Stack* stack){
-    return stack->top == MAX_SIZE-1;
+    return stack->top == stack->max_size-1;
 }
 
 int pop(Stack* stack){
@@ -36,26 +45,35 @@ void push(Stack* stack, int value){
     if(!is_full(stack)){
         stack->arr[++stack->top] = value;
     }    
-    else{
+    else if (!stack->is_dynamic){
         printf("%s", "stack overflow\n");
     }    
+    else{
+        stack->arr = realloc(stack->arr, stack->max_size *= 2);
+        stack->arr[++stack->top] = value;
+    }
 }    
 
 int peek(Stack* stack){
     return stack->arr[stack->top];
 }
 
+void print_stack(Stack* stack){
+    for(int i = 0; i <= stack->top; ++i){
+        printf("%d\n", stack->arr[i]);
+    }
+}
+
 int main(){
     Stack stack;
-    init(&stack);
+    init(&stack, 4, 1);
     
     push(&stack, 1234);
     push(&stack, 32);
     push(&stack, 78);
-    printf("%d\n", pop(&stack));
-    printf("%d\n", pop(&stack));
     push(&stack, 4);
-    printf("%d\n", pop(&stack));
+    push(&stack, 5);
     printf("%d\n", pop(&stack));
     
+    destroy(&stack);
 }
